aa4721d9b83dbd96f9125a2df05343c1a1668b52
Abraham-Dorsainvil <amswonadesodanodorsainvil.com>
2024-11-06 18:01:10 +0400

    Adding meta tag in the index.html, and create a new component folder called Components within it all components semantic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Nav from './Components/Nav'
 
 function App() {
 
 
   return (
     <>
-       <div>
-         <h3>HomePage</h3>
-       </div>
+       <Nav></Nav>
+       <Header></Header>
+       <Main></Main>
+       <Footer></Footer>
     </>
   )
 }
---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+        <h3>Main Section</h3>
+        <ul>
+            <li>Highlights</li>
+            <li>Testimonials</li>
+            <li>About</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Main
